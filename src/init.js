c27d76bc5359ba05512563ff927507ecb1f26ef2
Zalmoxisus <zalmoxisus@gmail.com>
2016-10-28 17:36:44 +0300

    Use with Redux DevTools Extension

--- src/init.js
+++ src/init.js
@@ -2,12 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { fromJS } from 'immutable';
 import App from './components/App';
 import rootReducer from './reducers';
 import 'styles/app.scss';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__({
+  deserializeState: (state) => ({
+    todos: {
+      ...state.todos,
+      todoList: fromJS(state.todos.todoList),
+    },
+  }),
+}));
+
+window.store = store;
 
 render(
   <Provider store={store}>
